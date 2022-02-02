e7738b47c8374550cc101223302a6c7394ffdfd1
Matias Borrazas <matias.borrazas@geopagos.com>
2022-02-02 17:19:06 -0300

    start

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React from "react";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { createBrowserHistory } from "history";
+import GreetingCat from "./GreetingCat";
+import RandomCat from "./RandomCat";
+
+const defaultHistory = createBrowserHistory();
+
+function App({ history = defaultHistory }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter history={history}>
+      <Routes>
+        <Route path="/" element={<RandomCat />}></Route>
+        <Route  exact path="/cat/:greeting" element={<GreetingCat history={history}/>} ></Route>
+       </Routes>
+     </BrowserRouter>
   );
 }
 
---
+++ src/GreetingCat.js
@@ -0,0 +1,25 @@
+import React from "react";
+import {
+  useParams
+} from "react-router-dom";
+
+export default function GreetingCat(props) {
+  const params = useParams();
+  const { greeting } = params;
+  const greetingCatUrl = `https://cataas.com/cat/says/${greeting}`;
+
+  return (
+    <div>
+      <header>
+        <h3>Greet me</h3>
+        {!greeting ? (
+          <div>Needs a greeting</div>
+        ) : (
+          <div>
+            <img src={greetingCatUrl} width="400px" alt="Cat" />
+          </div>
+        )}
+      </header>
+    </div>
+  );
+}
